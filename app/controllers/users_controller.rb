a9a6051b51fc90971e7447f65a2e12f2e843fd94
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-07-04 15:18:35 +0900

    Updateアクション、およびルーティングの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,33 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit]
+  before_action :set_user, only: [:show, :edit, :update]
   def show
   end
 
   def edit
   end
 
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to root_path, notice: 'User_Edit was successfully updated.' }
+        # format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        # format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
    def set_user
     @user = User.find(params[:id])
    end
+
+   def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :avatar
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts, except: :index
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 
   root "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
