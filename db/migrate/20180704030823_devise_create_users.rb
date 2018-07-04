e78fd42c489a1b41b5f3951667f86d3c016a0158
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-07-04 12:10:25 +0900

    DBの作成

--- db/migrate/20180704030823_devise_create_users.rb
+++ db/migrate/20180704030823_devise_create_users.rb
@@ -7,6 +7,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      t.string :name,               null: false, default: ""
+      t.text   :avatar
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180703081423) do
+ActiveRecord::Schema.define(version: 20180704030823) do
 
   create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
@@ -25,6 +25,25 @@ ActiveRecord::Schema.define(version: 20180703081423) do
     t.text     "content",    limit: 65535, null: false
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
+    t.string   "name",                                 default: "", null: false
+    t.text     "avatar",                 limit: 65535
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                        default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "images", "posts"
