f2fc6c4520ace03dd45bf04d6a068b33c9439408
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-07-04 17:21:07 +0900

    Userとprofile間にアソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   has_many :posts
+  has_one  :profile, dependent: :destroy
+
+  # 『belongs_to』にしてしまうと、『ActiveModel::MissingAttributeError』『can't write unknown attribute `profile_id`』のエラーが出る
+  # 1対1でも、参照先にidのカラムを持つテーブルが『belong_to』で記述し、参照されるテーブル側は『has_one』で記述する。（今回の場合、profileテーブルがuser_idカラムを持つ）
+
+  accepts_nested_attributes_for :profile
 
   mount_uploader :avatar, UserImageUploader
 
